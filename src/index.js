3a0b342526b5659fac667da768375741400aa024
eboka <ebokacp@yahoo.com>
2020-09-17 14:49:27 -0600

    hello eboka

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloEboka from './components/HelloEboka';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <> </>
+    <HelloEboka />
   </React.StrictMode>,
   document.getElementById('root')
 );
